363eff37d4332d3bfc42c0b30e86119c8c4fad02
Gabriel Ramos <grs17@aluno.ifnmg.edu.br>
2023-11-13 19:49:58 -0300

    initial

---
+++ src/main/java/com/mycompany/orm/persistencia/com/relacionamentos/ORMPERSISTENCIACOMRELACIONAMENTOS.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.orm.persistencia.com.relacionamentos;
+
+/**
+ *
+ * @author ramos
+ */
+public class ORMPERSISTENCIACOMRELACIONAMENTOS {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
